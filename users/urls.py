5741cea30290f2e82d7448a67edf8af94236842e
Oudone Phengkhamlar <oudonephengkhamlar@gmail.com>
2025-03-26 15:54:16 +0700

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,10 @@ from .views import (
     UserProfileView,
     request_otp,
     register_user,
-    LoginView
+    LoginView,
+    forgot_password_request,
+    reset_password,
+    change_password
 )
 
 urlpatterns = [
@@ -13,6 +16,11 @@ urlpatterns = [
     path('register/', register_user, name='register'),
     path('token/', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Password management
+    path('forgot-password/', forgot_password_request, name='forgot-password'),
+    path('reset-password/', reset_password, name='reset-password'),
+    path('change-password/', change_password, name='change-password'),
     
     # User URLs
     path('profile/', UserProfileView.as_view(), name='user-profile'),
