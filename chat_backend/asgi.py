1368caedf23117886a3b2682686e7d636105c908
Oudone Phengkhamlar <oudonephengkhamlar@gmail.com>
2025-05-02 13:15:46 +0700

    update for productions

--- chat_backend/asgi.py
+++ chat_backend/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
+django.setup()  # Initialize Django before importing the rest
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.jwt_middleware import JWTAuthMiddlewareStack
 from chat import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- deploy/gunicorn.conf.py
+++ deploy/gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:8001"  # Changed from 8000 to 8001
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 max_requests = 1000
